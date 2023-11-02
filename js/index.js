619211558e6728c7cb75b32c0fc90fa604528a6f
Afsanehaa <afsanehabdid@gmail.com>
2023-11-02 16:25:49 -0700

    statis counter numbers

--- js/index.js
+++ js/index.js
@@ -21,11 +21,54 @@ const swiperSlider = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
    
-  
-    // // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
   });
 
-  
+//  counter for statis box numbers
+
+const followersCount = document.querySelector('#statis-followers');
+
+let followersCounter = 0;
+const interval1 = setInterval(()=>{
+    followersCount.innerHTML = followersCounter;
+    followersCounter++
+
+    if (followersCounter === 1250){
+      clearInterval(interval1)
+    }
+},); 
+
+const classesCount = document.querySelector('#statis-classes');
+
+let classesCounter = 0;
+const interval2 = setInterval(()=>{
+    classesCount.innerHTML = classesCounter;
+    classesCounter++
+
+    if (classesCounter === 346){
+      clearInterval(interval2)
+    }
+},); 
+
+const studentsCount = document.querySelector('#statis-students');
+
+let studentsCounter = 0;
+const interval3 = setInterval(()=>{
+    studentsCount.innerHTML = studentsCounter;
+    studentsCounter++
+
+    if (studentsCounter === 210){
+      clearInterval(interval3)
+    }
+},); 
+
+const certifiedCount = document.querySelector('#statis-certified');
+
+let certifiedCounter = 0;
+const interval4 = setInterval(()=>{
+  certifiedCount.innerHTML = certifiedCounter;
+  certifiedCounter++
+
+    if (certifiedCounter === 508){
+      clearInterval(interval4)
+    }
+},); 
