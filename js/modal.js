9c044ec6cd4f409fecf8b45d3a6e93d338a36d50
Afsanehaa <afsanehabdid@gmail.com>
2023-11-02 14:36:25 -0700

    login modal

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,49 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// Get the modal---register modal
+var regmodal = document.getElementById("registerModal");
 
 // Get the button that opens the modal
-var btn = document.querySelector("#register-btn");
+var regbtn = document.querySelector("#register-btn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var logspan = document.getElementsByClassName("close2")[0];
+// Get the modal---login modal
+var logmodal = document.getElementById("loginModal");
+
+// Get the button that opens the modal
+var logbtn = document.querySelector("#login-btn");
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "flex";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+regbtn.onclick = function() {
+  regmodal.style.display = "flex";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    if (event.target == regmodal || event.target == logmodal) {
+      regmodal.style.display = "none";
+      logmodal.style.display = "none";
+    }
   }
+  
+  
+  // When the user clicks on the button, open the modal
+  logbtn.onclick = function() {
+    logmodal.style.display = "flex";
+  }
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+     regmodal.style.display = "none";
 }
+
+logspan.onclick = function(){
+    logmodal.style.display = "none";
+
+}
+
+
+
+
+
